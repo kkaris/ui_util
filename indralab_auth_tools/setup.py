f0a82568d0dc65443b2b4f03ba0f83b18f54b28a
kkaris <karis.klas@gmail.com>
2022-03-09 13:53:35 -0500

    Set version <4 for flask-jwt-extended

--- indralab_auth_tools/setup.py
+++ indralab_auth_tools/setup.py
@@ -2,15 +2,17 @@ from setuptools import setup, find_packages
 
 
 def main():
-    setup(name='indralab_auth_tools',
-          description='Authentication tools for indralab UIs.',
-          author='Patrick Greene',
-          author_email='patrick_greene@hms.harvard.edu',
-          url='http://github.com/indralab/ui_util',
-          packages=find_packages(),
-          install_requires=['flask-jwt-extended', 'flask', 'sqlalchemy', 'scrypt'],
-          include_package_data=True)
+    setup(
+        name="indralab_auth_tools",
+        description="Authentication tools for indralab UIs.",
+        author="Patrick Greene",
+        author_email="patrick_greene@hms.harvard.edu",
+        url="http://github.com/indralab/ui_util",
+        packages=find_packages(),
+        install_requires=["flask-jwt-extended<4.0.0", "flask", "sqlalchemy", "scrypt"],
+        include_package_data=True,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
